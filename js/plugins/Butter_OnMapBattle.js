f39e134716420e6f20eb7c121afc0ba8ea1883c4
ButterXant <steviegiovanni@gmail.com>
2021-04-11 08:55:32 -0700

    - copied some scene_battle code to scene_map - things are breaking :)

--- js/plugins/Butter_OnMapBattle.js
+++ js/plugins/Butter_OnMapBattle.js
@@ -1,9 +1,389 @@
 var Butter = Butter || {};
 Butter.OnMapBattle = Butter.OnMapBattle || {};
 
-Butter.OnMapBattle.StartBattle = function() {
-	console.log("starting battle");
-	BattleManager.playBattleBgm();
+BattleManager.isBusy = function() {
+    return ($gameMessage.isBusy() || /*this._spriteset.isBusy() ||*/
+            this._logWindow.isBusy());
+};
+
+Scene_Map.prototype.updateMain = function() {
+    var active = this.isActive();
+    $gameMap.update(active);
+    $gamePlayer.update(active);
+    $gameTimer.update(active);
+    $gameScreen.update();
+    if(BattleManager._phase != null && BattleManager._phase != 'init') {
+    	this.updateStatusWindow();
+	    this.updateWindowPositions();
+	    if (active && !this.isBusy()) {
+	        this.updateBattleProcess();
+	    }
+    }
+};
+
+Scene_Map.prototype.updateStatusWindow = function() {
+    if ($gameMessage.isBusy()) {
+        this._statusWindow.close();
+        this._partyCommandWindow.close();
+        this._actorCommandWindow.close();
+    } else if (this.isActive() && !this._messageWindow.isClosing()) {
+    	console.log(this);
+        this._statusWindow.open();
+    }
+};
+
+Scene_Map.prototype.updateWindowPositions = function() {
+    var statusX = 0;
+    if (BattleManager.isInputting()) {
+        statusX = this._partyCommandWindow.width;
+    } else {
+        statusX = this._partyCommandWindow.width / 2;
+    }
+    if (this._statusWindow.x < statusX) {
+        this._statusWindow.x += 16;
+        if (this._statusWindow.x > statusX) {
+            this._statusWindow.x = statusX;
+        }
+    }
+    if (this._statusWindow.x > statusX) {
+        this._statusWindow.x -= 16;
+        if (this._statusWindow.x < statusX) {
+            this._statusWindow.x = statusX;
+        }
+    }
+};
+
+Scene_Map.prototype.updateBattleProcess = function() {
+    if (!this.isAnyInputWindowActive() || BattleManager.isAborting() ||
+            BattleManager.isBattleEnd()) {
+        BattleManager.update();
+        this.changeInputWindow();
+    }
+};
+
+Scene_Map.prototype.isAnyInputWindowActive = function() {
+    return (this._partyCommandWindow.active ||
+            this._actorCommandWindow.active ||
+            this._skillWindow.active ||
+            this._itemWindow.active ||
+            this._actorWindow.active ||
+            this._enemyWindow.active);
+};
+
+/*Scene_Battle.prototype.update = function() {
+    var active = this.isActive();
+    this.updateStatusWindow();
+    this.updateWindowPositions();
+    if (active && !this.isBusy()) {
+        this.updateBattleProcess();
+    }
+};*/
+
+Game_Player.prototype.executeEncounter = function() {
+	if (!$gameMap.isEventRunning() && this._encounterCount <= 0) {
+		this.makeEncounterCount();
+		return true;
+    } else {
+    	return false;
+    }
+}
+
+Scene_Map.prototype.updateEncounter = function() {
+	if ($gamePlayer.executeEncounter()) {
+		this.CreateAllBattleWindow();
+		//SceneManager.push(Scene_Battle);
+	}       
+};
+
+Scene_Map.prototype.CreateAllBattleWindow = function() {
+	//this.createBattleSpriteset();
+	this.createLogWindow();
+    this.createStatusWindow();
+    this.createPartyCommandWindow();
+    this.createActorCommandWindow();
+    this.createHelpWindow();
+    this.createSkillWindow();
+    this.createItemWindow();
+    this.createActorWindow();
+    this.createEnemyWindow();
+    this.createMessageWindow();
+    this.createScrollTextWindow();
+    console.log(this);
+    BattleManager.setLogWindow(this._logWindow);
+    BattleManager.setStatusWindow(this._statusWindow);
+    //BattleManager.setSpriteset(this._spriteset);
+    //this._logWindow.setSpriteset(this._spriteset);
 	BattleManager.startBattle();
+}
+
+Scene_Map.prototype.createBattleSpriteset = function() {
+    this._battleSpriteset = new Spriteset_Battle();
+    this.addChild(this._battleSpriteset);
+};
+
+Scene_Map.prototype.createLogWindow = function() {
+    this._logWindow = new Window_BattleLog();
+    this.addWindow(this._logWindow);
+}
+
+Scene_Map.prototype.createStatusWindow = function() {
+    this._statusWindow = new Window_BattleStatus();
+    this.addWindow(this._statusWindow);
+};
+
+Scene_Map.prototype.createPartyCommandWindow = function() {
+    this._partyCommandWindow = new Window_PartyCommand();
+    this._partyCommandWindow.setHandler('fight',  this.commandFight.bind(this));
+    this._partyCommandWindow.setHandler('escape', this.commandEscape.bind(this));
+    this._partyCommandWindow.deselect();
+    this.addWindow(this._partyCommandWindow);
+};
+
+Scene_Map.prototype.commandFight = function() {
+    this.selectNextCommand();
+};
+
+Scene_Map.prototype.commandEscape = function() {
+    BattleManager.processEscape();
+    this.changeInputWindow();
+};
+
+Scene_Map.prototype.selectNextCommand = function() {
+    BattleManager.selectNextCommand();
+    this.changeInputWindow();
+};
+
+Scene_Map.prototype.changeInputWindow = function() {
+    if (BattleManager.isInputting()) {
+        if (BattleManager.actor()) {
+            this.startActorCommandSelection();
+        } else {
+            this.startPartyCommandSelection();
+        }
+    } else {
+        this.endCommandSelection();
+    }
+};
+
+Scene_Map.prototype.startActorCommandSelection = function() {
+    this._statusWindow.select(BattleManager.actor().index());
+    this._partyCommandWindow.close();
+    this._actorCommandWindow.setup(BattleManager.actor());
+};
+
+Scene_Map.prototype.startPartyCommandSelection = function() {
+    this.refreshStatus();
+    this._statusWindow.deselect();
+    this._statusWindow.open();
+    this._actorCommandWindow.close();
+    this._partyCommandWindow.setup();
+};
+
+Scene_Map.prototype.refreshStatus = function() {
+    this._statusWindow.refresh();
+};
+
+Scene_Map.prototype.endCommandSelection = function() {
+    this._partyCommandWindow.close();
+    this._actorCommandWindow.close();
+    this._statusWindow.deselect();
+};
+
+Scene_Map.prototype.createActorCommandWindow = function() {
+    this._actorCommandWindow = new Window_ActorCommand();
+    this._actorCommandWindow.setHandler('attack', this.commandAttack.bind(this));
+    this._actorCommandWindow.setHandler('skill',  this.commandSkill.bind(this));
+    this._actorCommandWindow.setHandler('guard',  this.commandGuard.bind(this));
+    this._actorCommandWindow.setHandler('item',   this.commandItem.bind(this));
+    this._actorCommandWindow.setHandler('cancel', this.selectPreviousCommand.bind(this));
+    this.addWindow(this._actorCommandWindow);
+};
+
+Scene_Map.prototype.commandAttack = function() {
+    BattleManager.inputtingAction().setAttack();
+    this.selectEnemySelection();
+};
+
+Scene_Map.prototype.selectEnemySelection = function() {
+    this._enemyWindow.refresh();
+    this._enemyWindow.show();
+    this._enemyWindow.select(0);
+    this._enemyWindow.activate();
+};
+
+Scene_Map.prototype.commandSkill = function() {
+    this._skillWindow.setActor(BattleManager.actor());
+    this._skillWindow.setStypeId(this._actorCommandWindow.currentExt());
+    this._skillWindow.refresh();
+    this._skillWindow.show();
+    this._skillWindow.activate();
+};
+
+Scene_Map.prototype.commandGuard = function() {
+    BattleManager.inputtingAction().setGuard();
+    this.selectNextCommand();
+};
+
+Scene_Map.prototype.commandItem = function() {
+    this._itemWindow.refresh();
+    this._itemWindow.show();
+    this._itemWindow.activate();
+};
+
+Scene_Map.prototype.selectPreviousCommand = function() {
+    BattleManager.selectPreviousCommand();
+    this.changeInputWindow();
+};
+
+Scene_Map.prototype.createHelpWindow = function() {
+    this._helpWindow = new Window_Help();
+    this._helpWindow.visible = false;
+    this.addWindow(this._helpWindow);
+};
+
+Scene_Map.prototype.createSkillWindow = function() {
+    var wy = this._helpWindow.y + this._helpWindow.height;
+    var wh = this._statusWindow.y - wy;
+    this._skillWindow = new Window_BattleSkill(0, wy, Graphics.boxWidth, wh);
+    this._skillWindow.setHelpWindow(this._helpWindow);
+    this._skillWindow.setHandler('ok',     this.onSkillOk.bind(this));
+    this._skillWindow.setHandler('cancel', this.onSkillCancel.bind(this));
+    this.addWindow(this._skillWindow);
+};
+
+Scene_Map.prototype.onSkillOk = function() {
+    var skill = this._skillWindow.item();
+    var action = BattleManager.inputtingAction();
+    action.setSkill(skill.id);
+    BattleManager.actor().setLastBattleSkill(skill);
+    this.onSelectAction();
+};
+
+Scene_Map.prototype.onSelectAction = function() {
+    var action = BattleManager.inputtingAction();
+    this._skillWindow.hide();
+    this._itemWindow.hide();
+    if (!action.needsSelection()) {
+        this.selectNextCommand();
+    } else if (action.isForOpponent()) {
+        this.selectEnemySelection();
+    } else {
+        this.selectActorSelection();
+    }
+};
+
+Scene_Map.prototype.onSkillCancel = function() {
+    this._skillWindow.hide();
+    this._actorCommandWindow.activate();
+};
+
+Scene_Map.prototype.createItemWindow = function() {
+    var wy = this._helpWindow.y + this._helpWindow.height;
+    var wh = this._statusWindow.y - wy;
+    this._itemWindow = new Window_BattleItem(0, wy, Graphics.boxWidth, wh);
+    this._itemWindow.setHelpWindow(this._helpWindow);
+    this._itemWindow.setHandler('ok',     this.onItemOk.bind(this));
+    this._itemWindow.setHandler('cancel', this.onItemCancel.bind(this));
+    this.addWindow(this._itemWindow);
+};
+
+Scene_Map.prototype.onItemOk = function() {
+    $gameParty.setLastItem(this.item());
+    this.determineItem();
+};
+
+Scene_Map.prototype.onItemCancel = function() {
+    this._itemWindow.deselect();
+    this._categoryWindow.activate();
+};
+
+Scene_Map.prototype.createActorWindow = function() {
+    this._actorWindow = new Window_BattleActor(0, this._statusWindow.y);
+    this._actorWindow.setHandler('ok',     this.onActorOk.bind(this));
+    this._actorWindow.setHandler('cancel', this.onActorCancel.bind(this));
+    this.addWindow(this._actorWindow);
+};
+
+Scene_Map.prototype.onActorOk = function() {
+    var action = BattleManager.inputtingAction();
+    action.setTarget(this._actorWindow.index());
+    this._actorWindow.hide();
+    this._skillWindow.hide();
+    this._itemWindow.hide();
+    this.selectNextCommand();
+};
+
+Scene_Map.prototype.onActorCancel = function() {
+    this._actorWindow.hide();
+    switch (this._actorCommandWindow.currentSymbol()) {
+    case 'skill':
+        this._skillWindow.show();
+        this._skillWindow.activate();
+        break;
+    case 'item':
+        this._itemWindow.show();
+        this._itemWindow.activate();
+        break;
+    }
+};
+
+Scene_Map.prototype.createEnemyWindow = function() {
+    this._enemyWindow = new Window_BattleEnemy(0, this._statusWindow.y);
+    this._enemyWindow.x = Graphics.boxWidth - this._enemyWindow.width;
+    this._enemyWindow.setHandler('ok',     this.onEnemyOk.bind(this));
+    this._enemyWindow.setHandler('cancel', this.onEnemyCancel.bind(this));
+    this.addWindow(this._enemyWindow);
+};
+
+Scene_Map.prototype.onEnemyOk = function() {
+    var action = BattleManager.inputtingAction();
+    action.setTarget(this._enemyWindow.enemyIndex());
+    this._enemyWindow.hide();
+    this._skillWindow.hide();
+    this._itemWindow.hide();
+    this.selectNextCommand();
+};
+
+Scene_Map.prototype.onEnemyCancel = function() {
+    this._enemyWindow.hide();
+    switch (this._actorCommandWindow.currentSymbol()) {
+    case 'attack':
+        this._actorCommandWindow.activate();
+        break;
+    case 'skill':
+        this._skillWindow.show();
+        this._skillWindow.activate();
+        break;
+    case 'item':
+        this._itemWindow.show();
+        this._itemWindow.activate();
+        break;
+    }
+};
+
+Scene_Map.prototype.createMessageWindow = function() {
+    this._messageWindow = new Window_Message();
+    this.addWindow(this._messageWindow);
+    this._messageWindow.subWindows().forEach(function(window) {
+        this.addWindow(window);
+    }, this);
+};
+
+Scene_Map.prototype.createScrollTextWindow = function() {
+    this._scrollTextWindow = new Window_ScrollText();
+    this.addWindow(this._scrollTextWindow);
+};
+
+Butter.OnMapBattle.InitiateBattle = function(event) {
+	var ev = event.event();
+	if (ev.note.match(/<(?:MONSTER ID):[ ](\d+)>/i)) {
+		var troopId = parseInt(RegExp.$1);
+		if ($dataTroops[troopId]) {
+            BattleManager.setup(troopId, true, false);
+            BattleManager.onEncounter();
+         	$gamePlayer._encounterCount = 0;
+			Yanfly.DespawnEventID(event.eventId());   
+        }
+	}
 }
 
